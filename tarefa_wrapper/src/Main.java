3771f4b1710a82a03a947ec7b9896fc19cc10eae
Morita <joao@teste.com>
2025-05-22 15:54:55 -0300

    Transforma primitivo em wrapper

---
+++ tarefa_wrapper/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int numeroPrimitivo = 10;
+        Integer numeroWrapper = Integer.valueOf(numeroPrimitivo);
+
+        // Exibindo os valores
+        System.out.println(numeroPrimitivo);
+        System.out.println(numeroWrapper);
+    }
+}
