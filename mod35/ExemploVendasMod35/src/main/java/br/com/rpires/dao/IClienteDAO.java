619b49170812f9e585118224106dcef75b793fb0
Morita <joao@teste.com>
2025-10-08 11:43:55 -0300

    mod 35

---
+++ mod35/ExemploVendasMod35/src/main/java/br/com/rpires/dao/IClienteDAO.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package br.com.rpires.dao;
+
+import br.com.rpires.dao.generic.IGenericDAO;
+
+/**
+ * @author rodrigo.pires
+ *
+ */
+public interface IClienteDAO extends IGenericDAO<Cliente, Long> {
+
+
+}
